2eb0dfa647126d1b8db13fec5faa75c5fd1da8ee
Michał Słota <michal.k.slota@gmail.com>
2022-11-14 21:19:07 +0100

    add app strict mode

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import Products from './components/Products/Products';
 import Header from './components/Header/Header';
 import Container from './components/Container/Container';
+import React from 'react';
 
 const App = () => {
 
   return (
-    <Container>
-      <Header />
-      <Products />
-    </Container>
+    <React.StrictMode>
+      <Container>
+        <Header />
+        <Products />
+      </Container>
+    </React.StrictMode>
   );
 }
 
